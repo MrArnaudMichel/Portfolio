3e49dcc605171e15a418605ec93aad7438ce002f
Arnaud Michel <87248697+MrArnaudMichel@users.noreply.github.com>
2024-03-12 20:29:10 +0000

    Finish ProjectPage.css

--- src/components/projects/Contributor.js
+++ src/components/projects/Contributor.js
@@ -3,9 +3,13 @@ import './css/Contributor.css';
 
 function Contributor({contributor}) {
     return (
-        <div className="contributor">
-            <img src={`${contributor.git}.png`} alt={contributor.name} />
-            <p className="contributor-name">{contributor.name}</p> {/* Ajoutez la classe CSS ici */}
+        <div className="content-container">
+            <div className="contributor">
+                <a href={contributor.git} target="_blank" rel="noopener noreferrer">
+                    <img src={`${contributor.git}.png`} alt={contributor.name}/>
+                </a>
+                <p className="contributor-name">{contributor.name}</p>
+            </div>
         </div>
     );
 }
