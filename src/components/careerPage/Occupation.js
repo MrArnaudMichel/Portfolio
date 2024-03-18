a9397cb5c78da49b7f53d552c4d4690938297c7d
Arnaud Michel <87248697+MrArnaudMichel@users.noreply.github.com>
2024-03-18 20:00:19 +0000

    Update Occupation.js visuals

--- src/components/careerPage/Occupation.js
+++ src/components/careerPage/Occupation.js
@@ -40,31 +40,33 @@ function Occupation({ t }) {
                     )}
                 </div>
             </div>
-            <div className="occupation-description" data-aos="fade-up">
-                <p dangerouslySetInnerHTML={{__html: t(occupation.description)}}></p>
-            </div>
-            {occupation.projects && (
-                <div className="occupation-projects" data-aos="fade-up">
-                    <h3 dangerouslySetInnerHTML={{__html: `${t("projects")}`}}></h3>
-                        {occupation.projects.map((project, index) => (
-                            <p key={index} onClick={() => navigate(`/projects/${project}`)}>{t(project)}</p>
-                        ))}
+            <div className={"occupation-content"} data-aos="fade-up">
+                <div className="occupation-description" data-aos="fade-up">
+                    <p dangerouslySetInnerHTML={{__html: t(occupation.description)}}></p>
                 </div>
-            )}
-            {occupation.skills && (
-                <div className={"occupation-skills"} data-aos="fade-up">
-                    <h3 className={"title"} dangerouslySetInnerHTML={{__html: `${t("skills")}`}}></h3>
-                    <div className={"skills-tools tools"}>
-                        {occupation.skills.map((tool) => {
-                            return (
-                                <div className={"icons"}>
-                                    <p dangerouslySetInnerHTML={{__html: t(tool)}}></p>
-                                </div>
-                            );
-                        })}
+                {occupation.projects && (
+                    <div className="occupation-projects" data-aos="fade-up">
+                        <h3 dangerouslySetInnerHTML={{__html: `${t("projects")}`}}></h3>
+                            {occupation.projects.map((project, index) => (
+                                <p key={index} onClick={() => navigate(`/projects/${project}`)}>{t(project)}</p>
+                            ))}
                     </div>
-                </div>
-            )}
+                )}
+                {occupation.skills && (
+                    <div className={"occupation-skills"} data-aos="fade-up">
+                        <h3 className={"title"} dangerouslySetInnerHTML={{__html: `${t("skills")}`}}></h3>
+                        <div className={"skills-tools tools"}>
+                            {occupation.skills.map((tool) => {
+                                return (
+                                    <div className={"icons"}>
+                                        <p dangerouslySetInnerHTML={{__html: t(tool)}}></p>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+            </div>
             <div className={"back"} data-aos="fade-up">
                 <button onClick={() => navigate(-1)} dangerouslySetInnerHTML={{__html: t("back")}}></button>
             </div>
